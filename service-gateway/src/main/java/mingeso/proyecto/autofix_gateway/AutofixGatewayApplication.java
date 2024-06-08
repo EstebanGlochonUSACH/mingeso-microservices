a36a2b21d5287f4310840f16b4c877dc84c2407e
Esteban Glochon <esteban.glochon@gmail.com>
2024-06-08 17:37:35 -0400

    created api-gateway service

---
+++ service-gateway/src/main/java/mingeso/proyecto/autofix_gateway/AutofixGatewayApplication.java
@@ -0,0 +1,13 @@
+package mingeso.proyecto.autofix_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutofixGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutofixGatewayApplication.class, args);
+	}
+
+}
---
+++ service-gateway/src/test/java/mingeso/proyecto/autofix_gateway/AutofixGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package mingeso.proyecto.autofix_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutofixGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
