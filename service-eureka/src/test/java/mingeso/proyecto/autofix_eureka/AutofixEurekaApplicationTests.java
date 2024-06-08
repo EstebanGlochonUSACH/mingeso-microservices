f74b3fc4f835d2f89b610e96fede8b80e59fa711
Esteban Glochon <esteban.glochon@gmail.com>
2024-06-08 17:26:04 -0400

    create service eureka

---
+++ service-eureka/src/test/java/mingeso/proyecto/autofix_eureka/AutofixEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package mingeso.proyecto.autofix_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutofixEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
