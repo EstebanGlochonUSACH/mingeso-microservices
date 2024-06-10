91798f2045b37e1edef25e8b9cb6f19531fad428
Esteban Glochon <esteban.glochon@gmail.com>
2024-06-10 00:26:41 -0400

    update service reportes

--- service-reportes/src/main/java/mingeso/proyecto/autofix_reportes/AutofixReportesApplication.java
+++ service-reportes/src/main/java/mingeso/proyecto/autofix_reportes/AutofixReportesApplication.java
@@ -2,13 +2,14 @@ package mingeso.proyecto.autofix_reportes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableFeignClients
 public class AutofixReportesApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		SpringApplication.run(AutofixReportesApplication.class, args);
 	}
 
