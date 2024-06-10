c47bf369f08e1c37863fe371ce678b2a249df796
Esteban Glochon <esteban.glochon@gmail.com>
2024-06-10 11:22:50 -0400

    user frontend as gateway for testing

--- service-frontend/vite.config.ts
+++ service-frontend/vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const BACKEND_URI = process.env.BACKEND_URI ? process.env.BACKEND_URI : 'http://localhost:8080';
+const SERVICES_AUTOS_URI = process.env.SERVICES_AUTOS_URI ? process.env.SERVICES_AUTOS_URI : 'http://localhost:8090';
+const SERVICES_REPARACIONES_URI = process.env.SERVICES_REPARACIONES_URI ? process.env.SERVICES_REPARACIONES_URI : 'http://localhost:8091';
+const SERVICES_ORDENES_URI = process.env.SERVICES_ORDENES_URI ? process.env.SERVICES_ORDENES_URI : 'http://localhost:8092';
+const SERVICES_REPORTES_URI = process.env.SERVICES_REPORTES_URI ? process.env.SERVICES_REPORTES_URI : 'http://localhost:8093';
+
 // https://vitejs.dev/config/
 export default defineConfig({
 	server: {
@@ -8,8 +14,38 @@ export default defineConfig({
 		port: 3000,
 		strictPort: true,
 		proxy: {
-			'/api': {
-				target: process.env.BACKEND_URI ? process.env.BACKEND_URI : 'http://localhost:8080',
+			// '/api': {
+			//	target: BACKEND_URI,
+			//	changeOrigin: true,
+			//	rewrite: (path) => path.replace(/^\/api\//, '/'),
+			// },
+			'/api/autos': {
+				target: SERVICES_AUTOS_URI,
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api\//, '/'),
+			},
+			'/api/marcas': {
+				target: SERVICES_AUTOS_URI,
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api\//, '/'),
+			},
+			'/api/reparaciones': {
+				target: SERVICES_REPARACIONES_URI,
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api\//, '/'),
+			},
+			'/api/ordenes': {
+				target: SERVICES_ORDENES_URI,
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api\//, '/'),
+			},
+			'/api/bonos': {
+				target: SERVICES_ORDENES_URI,
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api\//, '/'),
+			},
+			'/api/reportes': {
+				target: SERVICES_REPORTES_URI,
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/api\//, '/'),
 			},
