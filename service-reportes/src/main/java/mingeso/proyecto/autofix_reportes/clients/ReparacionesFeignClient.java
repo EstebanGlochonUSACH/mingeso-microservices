1e94967563f2d8b2ea03c28ded2b336d66f2522e
Esteban Glochon <esteban.glochon@gmail.com>
2024-07-12 16:20:33 -0400

    small warning fix

--- service-reportes/src/main/java/mingeso/proyecto/autofix_reportes/clients/ReparacionesFeignClient.java
+++ service-reportes/src/main/java/mingeso/proyecto/autofix_reportes/clients/ReparacionesFeignClient.java
@@ -2,7 +2,6 @@ package mingeso.proyecto.autofix_reportes.clients;
 
 import mingeso.proyecto.autofix_reportes.dtos.ReparacionTipoDTO;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
